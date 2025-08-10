de9348304649aa3a387a947ccb5b28793c8cf958
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-08-10 02:36:51 -0700

    Update test_arch.sh

--- s1/test_arch.sh
+++ s1/test_arch.sh
@@ -12,6 +12,8 @@ if [ "$dataset" == "cifar10" ]; then
   python ./s1/test_cifar10.py --batch_size ${batch_size} --data ${data_path} --dir ${genotype_dir} --auxiliary --model_path ${model_path} --report_freq 100 --log_path ${log_path} --gpu ${gpu}
 elif [ "$dataset" == "cifar100" ]; then
   python ./s1/test_cifar100.py --batch_size ${batch_size} --data ${data_path} --dir ${genotype_dir} --auxiliary --model_path ${model_path} --report_freq 100 --log_path ${log_path} --gpu ${gpu}
+elif [ "$dataset" == "jaffe7" ]; then
+  python ./s1/test_jaffe7.py --batch_size ${batch_size} --data ${data_path} --dir ${genotype_dir} --auxiliary --model_path ${model_path} --report_freq 100 --log_path ${log_path} --gpu ${gpu}
 else
   python ./s1/test_imagenet.py --batch_size ${batch_size} --data ${data_path} --dir ${genotype_dir} --auxiliary --model_path ${model_path} --report_freq 100
 fi
