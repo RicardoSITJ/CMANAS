e47236ad34ea587d298ebf0d7043e266d77b8db2
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-08-10 18:56:29 -0700

    Update eval_arch.sh

--- s1/eval_arch.sh
+++ s1/eval_arch.sh
@@ -16,6 +16,8 @@ if [ "$dataset" == "cifar10" ] ; then
   python ./s1/train_cifar10.py --cutout --auxiliary --epochs ${epochs} --dir ${arch_dir} --data ${data_path} --gpu ${gpu}
 elif [ "$dataset" == "jaffe7" ] ; then
   python ./s1/train_jaffe7.py --cutout --auxiliary --epochs ${epochs} --dir ${arch_dir} --data ${data_path} --gpu ${gpu}
+elif [ "$dataset" == "ckplus" ] ; then
+  python ./s1/train_ckplus.py --cutout --auxiliary --epochs ${epochs} --dir ${arch_dir} --data ${data_path} --gpu ${gpu}
 else
   python ./s1/train_cifar100.py --cutout --auxiliary --epochs ${epochs} --dir ${arch_dir} --data ${data_path} --gpu ${gpu}
 fi
