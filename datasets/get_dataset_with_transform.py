9d2a681b52036b33d6d3f547fbb086dac2d91821
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-01 16:49:33 -0700

    Update get_dataset_with_transform.py

--- datasets/get_dataset_with_transform.py
+++ datasets/get_dataset_with_transform.py
@@ -267,7 +267,7 @@ def get_datasets(name, root, cutout):
         test_data = ImageNet16(root, False, test_transform, 200)
         assert len(train_data) == 254775 and len(test_data) == 10000
     elif name == "jaffe7":
-        folder_path = "/kaggle/working/CMANAS/datasets/jaffe_dataset"
+        folder_path = "/kaggle/working/CMANAS/datasets/loso/excluded_KA"
         train_data = dset.ImageFolder(osp.join(folder_path, "train"), train_transform)
         test_data = dset.ImageFolder(osp.join(folder_path, "val"), test_transform)
     elif name == "ckplus_2":
