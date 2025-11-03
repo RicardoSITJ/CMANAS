370a99b8bc9e4fb150ee5ee18302a918657d40bb
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-03 08:05:49 -0800

    gg

--- s1/train_jaffe7.py
+++ s1/train_jaffe7.py
@@ -75,7 +75,8 @@ args = parser.parse_args()
 
 if args.seed is None or args.seed < 0:
     args.seed = random.randint(1, 100000)
-args.save = "eval-{}-{}".format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+# args.save = "eval-{}-{}".format(args.save, time.strftime("%Y%m%d-%H%M%S"))
+args.save = "eval-{}-{}".format(args.save, 1)
 if args.dir is not None:
     args.save = os.path.join(args.dir, args.save)
 ut.create_exp_dir(args.save)
