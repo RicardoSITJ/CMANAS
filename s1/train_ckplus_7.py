96860b4a2b6757c9493bb50ecbf98868e7a6a162
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-06 09:03:37 -0800

    Update train_ckplus_7.py

--- s1/train_ckplus_7.py
+++ s1/train_ckplus_7.py
@@ -261,7 +261,10 @@ def train(train_queue, model, criterion, optimizer):
         top5.update(prec5.data, n)
 
         if step % args.report_freq == 0:
-            logging.info("train %03d %e %f %f", step, objs.avg, top1.avg, top5.avg)
+            # logging.info("train %03d %e %f %f", step, objs.avg, top1.avg, top5.avg)
+            logging.info(
+                f"train {step:03d} {objs.avg:.5e} {top1.avg:.4f} {top5.avg:.4f}"
+            )
 
     return top1.avg, objs.avg
 
