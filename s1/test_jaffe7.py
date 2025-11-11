c9a2409dda2cfaba36168fa1b02fa76ba38a9b4b
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-10 22:49:57 -0800

    Update test_jaffe7.py

--- s1/test_jaffe7.py
+++ s1/test_jaffe7.py
@@ -125,7 +125,7 @@ def main():
 
     model.drop_path_prob = 0.0
     # model.drop_path_prob = args.drop_path_prob
-    test_acc, test_obj = infer_n_times(test_queue, model, criterion)
+    test_acc, test_obj, fps = infer_n_times(test_queue, model, criterion)
     logging.info("test_acc %f", test_acc)
     print(f"test_acc {test_acc}")
 
