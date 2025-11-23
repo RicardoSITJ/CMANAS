edb96680b20f77e97ab991fa7764cb2bb4c1fad4
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-22 22:55:50 -0800

    Update train_finetune_jaffe.py

--- s1/train_finetune_jaffe.py
+++ s1/train_finetune_jaffe.py
@@ -57,7 +57,8 @@ args = parser.parse_args()
 if args.seed is None or args.seed < 0:
     args.seed = random.randint(1, 100000)
 
-args.save = f"eval-{args.save}-{args.seed}"
+subject = args.data_dir.rstrip("/").split("/")[-1]
+args.save = f"eval-{args.save}-{subject}-{args.seed}-{args.epochs}"
 if args.dir is not None:
     args.save = os.path.join(args.dir, args.save)
 ut.create_exp_dir(args.save)
