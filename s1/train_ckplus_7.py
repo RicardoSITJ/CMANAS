cd20a63aa375815bcb8cfbf421b038b627e88d9f
RicardoSITJ <165713228+RicardoSITJ@users.noreply.github.com>
2025-11-30 10:48:23 -0800

    Update train_ckplus_7.py

--- s1/train_ckplus_7.py
+++ s1/train_ckplus_7.py
@@ -68,7 +68,7 @@ parser.add_argument(
     "--drop_path_prob", type=float, default=0.2, help="drop path probability"
 )
 parser.add_argument("--save", type=str, default="EXP", help="experiment name")
-parser.add_argument("--seed", type=int, default=42, help="random seed")
+parser.add_argument("--seed", type=int, default=4000, help="random seed")
 parser.add_argument(
     "--arch", type=str, default="DARTS", help="which architecture to use"
 )
